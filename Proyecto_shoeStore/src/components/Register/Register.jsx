410e5e16830227bb890cf3acf55a58d8e6d5cfc2
Ayelen Quispe <quispeaguilar.bel3n@gmail.com>
2025-09-17 12:00:58 -0300

    Modifique y mejore el archivo Register.jsx

--- Proyecto_shoeStore/src/components/Register/Register.jsx
+++ Proyecto_shoeStore/src/components/Register/Register.jsx
@@ -1,36 +1,48 @@
 import React, { useState } from "react";
+import './Register.css'
 
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Datos del registro:", { email, password });
+
+    if (email === "" || password == ""){
+      setMessage(`Complete con un email o contraseña`)
+    }
+    else{
+      setMessage(`Bienvenido`)
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit} style={{ maxWidth: "300px", margin: "20px auto" }}>
-      <h2>Registro</h2>
-      <input
-        type="email"
-        placeholder="Ingresa tu correo"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        style={{ display: "block", margin: "10px 0", padding: "8px", width: "100%" }}
-      />
-      
-      <input
-        type="password"
-        placeholder="Ingresa tu contraseña"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        style={{ display: "block", margin: "10px 0", padding: "8px", width: "100%" }}
-      />
-      
-      <button type="submit" style={{ padding: "8px", width: "100%" }}>
-        Registrarse
-      </button>
-    </form>
+    <div class = "register-container">
+      <form class = "form" onSubmit={handleSubmit}>
+        <h2 class = "register-h2">Registro</h2>
+
+        <input class = "input"
+          type="email"
+          placeholder="Ingresa tu correo"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        
+        <input class = "input"
+          type="password"
+          placeholder="Ingresa tu contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}    
+        />
+        
+        <button class = "button">
+          Registrarse
+        </button>
+        {message && <p class = "message">{message}</p>}
+      </form>
+    </div>
   );
 }
+/////////
