64c73eb367778e75c2a018d84fc826378aafc52f
Ayelen Quispe <quispeaguilar.bel3n@gmail.com>
2025-09-24 13:51:04 -0300

    Organizamos register y login en la carpeta Paginas

--- Proyecto_shoeStore/src/App.jsx
+++ Proyecto_shoeStore/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Login from './components/login/login'
+import Login from './Paginas/Login/login'
+import Register from './Paginas/Register/Register'
 import Header from "./components/Header/Header"
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
         <h2>Hola</h2>
         <p>La info</p>
       </main>
-
+    <Footer/>
     </>
   )
 }
