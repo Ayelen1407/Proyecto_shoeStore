698a6a7eaf11ab231feea012a32338e039a3158c
Ariana Villa <arianavilla4817@gmail.com>
2025-09-25 12:30:23 -0300

    hice endpoints, en la que muestra e inserta, cuatro en cada una (tablas: shoes,clientes, empleados)

--- AGBD/app.py
+++ AGBD/app.py
@@ -21,35 +21,123 @@ def cerrarConexion(e=None):
     if db is not None:
         db.close()
         
-#muestra producto 
-@app.route('/shoes')
+#muestra tabla shoes. /api porque se conecta con lo de fede
+@app.route('/api/shoes', methods=['GET'])
 def mostrar_shoes():
-    db = abrirConexion()
-    cursor = db.cursor(dictionary=True)  # dictionary=True devuelve filas como diccionarios
-    cursor.execute("SELECT * FROM shoes")  # productos es tu tabla
-    resultados = cursor.fetchall()  # trae todas las filas
-    cursor.close()
-    cerrarConexion()
-    
-    return jsonify(resultados)  # por ahora devolvemos los resultados como texto
+   db = abrirConexion()
+   cursor = db.cursor(dictionary=True)  # dictionary=True devuelve filas como diccionarios (columna : valor)
+   cursor.execute("SELECT * FROM shoes")  # productos es tu tabla
+   resultados = cursor.fetchall()  # trae todas las filas/registros
+   cursor.close()
+   cerrarConexion()
+   return {"shoes": resultados}  #devolve los resultados como texto
 
-@app.route('/agregar_shoes/<int:id_shoes>/<int:id_talles>/<marca>/<nombre>/<precio>/<tipo>', )
-def agregar_shoes(id_shoes, id_talles, marca, nombre, precio, tipo):
-    db = abrirConexion()
-    cursor = db.cursor()
-    sql = "INSERT INTO productos (id_shoes, id_talles, marca, nombre, precio, tipo) VALUES (%s, %s, %s, %s, %s, %s)"
-    valores = (id_shoes, id_talles, marca, nombre, precio, tipo)
-    cursor.execute(sql, valores)
-    db.commit()  # confirma los cambios en la base
-    cursor.close()
-    cerrarConexion()
-    
-    return f"Producto {nombre} agregado correctamente."
+#muestra tabla clientes
+@app.route('/clientes', methods=['GET'])
+def mostrar_clientes():
+   db = abrirConexion()
+   cursor = db.cursor(dictionary=True)
+   cursor.execute("SELECT * FROM clientes") 
+   resultados = cursor.fetchall()
+   cursor.close()
+   cerrarConexion()
+   return {"clientes": resultados}
+
+#muestra tabla empleados
+@app.route('/empleados', methods=['GET'])
+def mostrar_empleados():
+   db = abrirConexion()
+   cursor = db.cursor(dictionary=True)
+   cursor.execute("SELECT * FROM empleados") 
+   resultados = cursor.fetchall() 
+   cursor.close()
+   cerrarConexion()
+   return {"clientes": resultados}
+
+#muestra tabla "cantidad_talles"
+@app.route('/talles', methods=['GET'])
+def mostrar_cantidad_Talles():
+   db = abrirConexion()
+   cursor = db.cursor(dictionary=True)
+   cursor.execute("SELECT * FROM cantidad_talles") 
+   resultados = cursor.fetchall() 
+   cursor.close()
+   cerrarConexion()
+   return {"cantidad_talles": resultados}
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
+
+#inserta/agrega datos en la tabla "cantidad talles".
+@app.route('/agregarTalle', methods=['POST'])
+def agregar_talle():
+   data = request.json
+   talle = data['talle']
+   db = abrirConexion()
+   cursor = db.cursor()
+   cursor.execute("INSERT INTO cantidad_talles (talle) VALUES (%s)", (talle,))
+   db.commit()
+   cursor.close()
+   cerrarConexion()
+   return {"mensaje": "talle agregado"}
+
+#inserta/agrega datos en la tabla shoes
+@app.route('/agregarShoes', methods = ['POST'])
+def agregar_shoes():
+   data = request.json
+   nombre = data['nombre']
+   tipo = data['tipo']
+   marca = data['marca']
+   precio = data['precio']
+   id_talles = data['id_talles']
+   db = abrirConexion()
+   cursor = db.cursor()
+   cursor.execute("INSERT INTO shoes (nombre,tipo,marca,precio,id_talles) VALUES (%s,%s,%s,%s,%s)", (nombre,tipo,marca,precio,id_talles))
+   db.commit()  # confirma los cambios en la base
+   cursor.close()
+   cerrarConexion()
+   return {"mensaje": f"Producto {nombre} agregado"}
+
+#inserta/agrega datos en la tabla clientes
+@app.route('/agregarClientes', methods = ['POST'])
+def agregar_cliente():
+   data = request.json
+   nombre = data['nombre']
+   apellido = data['apellido']
+   gasto = data['gasto']
+   direccion = data['direccion']
+   email = data['email']
+   numero = data['numero']
+   id_genero = data['id_genero']
+   id_shoes = data['id_shoes']
+   id_empleados = data['id_empleados']
+   db = abrirConexion()
+   cursor = db.cursor()
+   cursor.execute("INSERT INTO clientes (nombre,apellido,gasto,direccion,email,numero,id_genero,id_shoes,id_empleados) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)", (nombre, apellido, gasto,direccion,email,numero,id_genero,id_shoes,id_empleados))
+   db.commit()
+   cursor.close()
+   cerrarConexion()
+   return {"mensaje": f"Cliente {apellido} agregado"}
+
+#inserta/agrega datos en la tabla empleados
+@app.route('/agregarEmpleado', methods = ['POST'])
+def agregar_empleado():
+   data = request.json
+   nombre = data['nombre']
+   apellido = data['apellido']
+   edad = data['edad']
+   puesto = data['puesto']
+   db = abrirConexion()
+   cursor = db.cursor()
+   cursor.execute("INSERT INTO empleados (nombre,apellido,edad,puesto) VALUES (%s,%s,%s,%s)", (nombre, apellido, edad, puesto))
+   db.commit()
+   cursor.close()
+   cerrarConexion()
+   return {"mensaje": f"Empleado {apellido} agregado"}
+
+
+
+#AYE
 #Consulta para obtener todos los clientes
 @app.route('/clientes', methods=['GET'])
 def get_shoes():
