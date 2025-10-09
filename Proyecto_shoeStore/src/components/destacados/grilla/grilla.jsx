4172204c3bbbce7788b7c17089b6fd3f74e8b93c
Ariana Villa <arianavilla4817@gmail.com>
2025-10-09 12:12:47 -0300

    modifique algunos links y nombre de la grilla

--- Proyecto_shoeStore/src/components/destacados/grilla/grilla.jsx
+++ Proyecto_shoeStore/src/components/destacados/grilla/grilla.jsx
@@ -4,8 +4,8 @@ import './grilla.css';
 const products = [
   { id: 1, name: 'Samba OG', marca: 'Adidas', image: 'https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/3bbecbdf584e40398446a8bf0117cf62_9366/Zapatillas_Samba_OG_Blanco_B75806_01_00_standard.jpg', price: '$160.000' },
   { id: 2, name: 'Amplimove', marca: 'Adidas', image: 'https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/c8d5b05212c245408df30ce9075a437c_9366/Zapatillas_de_Entrenamiento_Amplimove_Blanco_JR9298_01_00_standard.jpg', price: '$100.000' },
-  { id: 3, name: 'Air Force', marca: 'Nike', image: 'https://nikearprod.vtexassets.com/arquivos/ids/163455-800-800?width=800&height=800&aspect=true', price: '$200.000' },
-  { id: 4, name: 'Legend Essential', marca: 'Nike', image: 'https://nikearprod.vtexassets.com/arquivos/ids/501786-800-800?width=800&height=800&aspect=true', price: '$120.000' },
+  { id: 3, name: 'MC trainer', marca: 'Nike', image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rebelsport.com.au%2Fdw%2Fimage%2Fv2%2FBBRV_PRD%2Fon%2Fdemandware.static%2F-%2FSites-srg-internal-master-catalog%2Fdefault%2Fdwf095524e%2Fimages%2F68503501%2FRebel_68503501_blackwhite_hi-res.jpg%3Fsw%3D1000%26sh%3D1000%26sm%3Dfit%26q%3D70&f=1&nofb=1&ipt=cdbe33beecbca1d067422d3c7339f93827faa4abdc5d66f61b02e2914d32d525', price: '$120.000' },
+  { id: 4, name: 'Dunk low retro', marca: 'Nike', image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsneakerbaker.com%2Fwp-content%2Fuploads%2F2021%2F11%2FNike-Dunk-Low-Retro-White-Black-2021-3.jpg&f=1&nofb=1&ipt=4caadcb2b5d4646411e5ee69a676b6f41575fa7491ce2683fd9fc14b5f2e7005', price: '$160.000' },
 ];
 
 const Grilla = () => {
