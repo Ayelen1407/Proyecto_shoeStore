7809f33f25e22e1667cd619d449b6c2105e9b729
Ayelen Quispe <quispeaguilar.bel3n@gmail.com>
2025-10-16 12:31:12 -0300

    Agregue unos detalles a los componentes de cada zapatilla y puse una imagen a la pagina principal

--- Proyecto_shoeStore/src/App.jsx
+++ Proyecto_shoeStore/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
       <>
         <Header/>
-        <h1>Página shoeStore</h1>
+        <img className='img-principal' src='https://raphael.app/api/proxy-image/7ff5475d-5394-4c34-9852-c1a83370ede2.webp'></img>
         <main className="main">
         </main>
         <div className="contenedor-principal">
--- Proyecto_shoeStore/src/components/zapatillasHightop/high-top.jsx
+++ Proyecto_shoeStore/src/components/zapatillasHightop/high-top.jsx
@@ -6,7 +6,7 @@ export default function HighTop() {
     <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F42%2F8f%2F3c%2F428f3cfa12051c64a426327c609646c0.jpg&f=1&nofb=1&ipt=1601c37d01a797f85805a02da6f43436ee88023658941afdb115e25b18d84757" 
     alt="Zapatillas High-top" />
     <div className="Zapatilla deportiva">
-      <h2>HIGH-TOP</h2>
+      <h2 className="descripcion-h2">HIGH-TOP</h2>
       <button>VER MÁS</button>
     </div>
   </div>
